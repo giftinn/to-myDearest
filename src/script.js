6196788aef3f25a91c9178f22eb5a36ad40be362
giftin.id <calistanafisaa@gmail.com>
2026-03-27 11:42:58 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -16,8 +16,8 @@ html:`
 <div class="songCard">
 <img class="cover" src="src/pict.jpg">
 <div class="songInfo">
-<div>Waltz for Moon</div>
-<div style="font-size:12px;opacity:.7;margin-bottom:10px">Hodge</div>
+<div>Home</div>
+<div style="font-size:12px;opacity:.7;margin-bottom:10px">Bruno Major</div>
 
 <div class="player">
 <div class="topRow">
